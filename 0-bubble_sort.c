8cbfd5d9f5c9e288e30aaa6792c8b31bd8ed4d4b
qkyo1 <engawab8@gmail.com>
2024-02-21 15:19:48 +0400

    trying fixing 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t x, y;
 	int temp;
-	int swap;
 
 	for (x = 0; x < size - 1; x++)
 	{
-		swap = 0;
 		for (y = 0; y < size - x - 1; y++)
 		{
 			if (array[y] > array[y + 1])
@@ -22,12 +20,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[y];
 				array[y] = array[y + 1];
 				array[y + 1] = temp;
-				swap = 1;
 				print_array(array, size);
 			}
 		}
-
-		if (swap == 0)
-			break;
 	}
 }
