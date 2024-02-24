05b20da97b24c1ab23c40f5608c1b6247b9c425f
qkyo1 <engawab8@gmail.com>
2024-02-24 19:02:42 +0400

    fix 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,8 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - a function to sort double linked list
- * @list: lists y3ny 7 tkon $no
+* insertion_sort_list - a function to sort double linked list
+* @list: lists y3ny 7 tkon $no
 */
 
 void insertion_sort_list(listint_t **list)
@@ -17,25 +17,25 @@ while (curr != NULL)
 temp = curr;
 curr = curr->next;
 while (temp->prev != NULL && temp->n < temp->prev->n)
-	{
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->prev->next = temp->next;
+{
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
+	temp->prev->next = temp->next;
 
-			temp->next = temp->prev;
-			temp->prev = temp->prev->prev;
+	temp->next = temp->prev;
+	temp->prev = temp->prev->prev;
 
-			if (temp->prev != NULL)
-				temp->prev->next = temp;
-			else
-				*list = temp;
+	if (temp->prev != NULL)
+		temp->prev->next = temp;
+	else
+		*list = temp;
 
-			temp->next->prev = temp;
+	temp->next->prev = temp;
 
-			if (temp->prev == NULL)
-				*list = temp;
+	if (temp->prev == NULL)
+		*list = temp;
 
-			print_list(*list);
-		}
+	print_list(*list);
+}
 	}
 }
